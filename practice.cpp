8af7eee15161406e560c99cb76502f543a9ca664
imshree03 <shreebhosale24hrs@gmail.com>
2023-02-20 19:26:47 +0530

    First Commit for file practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int M = 1e9 + 7;
+int main()
+{
+    int N = 51;
+    long long factorial = 1;
+    for (int i = 2; i <= N; i++)
+    {
+        factorial = ((factorial) % M * (i) % M) % M;
+    }
+    cout << factorial << "\n";
+}
